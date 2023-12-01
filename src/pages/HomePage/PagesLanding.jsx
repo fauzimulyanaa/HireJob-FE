e2e29adefdca527ce57b138f6768bb265ac62e1a
dzhecky <habakiri76@gmail.com>
2023-12-02 00:56:57 +0700

    test landing

---
+++ src/pages/HomePage/PagesLanding.jsx
@@ -0,0 +1,9 @@
+import HomeLanding from "../../components/LandingPage/Landing";
+
+export default function HomeLandingPage(){
+    return(
+        <>
+        <HomeLanding />
+        </>
+    )
+}
